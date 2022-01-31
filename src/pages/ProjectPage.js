ec75c3ad272a1530949c86d4ba5cdf85c1116f78
Vincent Laquindanum <laq.vince@gmail.com>
2022-01-31 14:11:02 -0800

    add links to projects

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -33,22 +33,26 @@ const ProjectPage = () => {
                         </div>
                         
                         <div className='flex flex-row justify-end'>
-                            <div className='m-3'>
-                                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#a8b2d1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6">
-                                    <title>GitHub</title>
-                                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                                </svg>
-                                
-                            </div>
-                            <div className='ml-3 my-3'>
-                                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#a8b2d1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6">
-                                    <title>External Link</title>
-                                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                                    <polyline points="15 3 21 3 21 9"></polyline>
-                                    <line x1="10" y1="14" x2="21" y2="3"></line>
-                                </svg>
-                                
-                            </div>
+                            <a href="https://github.com/vincelaq/retro-future">
+                                <div className='m-3'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#a8b2d1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6">
+                                        <title>GitHub</title>
+                                        <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                                    </svg>
+                                    
+                                </div>
+                            </a>
+                            <a href="http://retro-future.herokuapp.com/">
+                                <div className='ml-3 my-3'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#a8b2d1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6">
+                                        <title>External Link</title>
+                                        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                                        <polyline points="15 3 21 3 21 9"></polyline>
+                                        <line x1="10" y1="14" x2="21" y2="3"></line>
+                                    </svg>
+                                    
+                                </div>
+                            </a>
                         </div>
                     </div>
                     <div className='project-image-second '>
@@ -85,22 +89,25 @@ const ProjectPage = () => {
                         </div>
                         
                         <div className='flex flex-row justify-start'>
-                            <div className='my-3 mr-3'>
-                                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#a8b2d1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6">
-                                    <title>GitHub</title>
-                                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                                </svg>
-                                
-                            </div>
-                            <div className='m-3'>
-                                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#a8b2d1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6">
-                                    <title>External Link</title>
-                                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                                    <polyline points="15 3 21 3 21 9"></polyline>
-                                    <line x1="10" y1="14" x2="21" y2="3"></line>
-                                </svg>
-                                
-                            </div>
+                            <a href="https://github.com/vincelaq/dev-social-media">
+                                <div className='my-3 mr-3'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#a8b2d1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6">
+                                        <title>GitHub</title>
+                                        <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                                    </svg>
+                                    
+                                </div>
+                            </a>
+                            <a href="https://devbook-flex525.herokuapp.com/">
+                                <div className='m-3'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#a8b2d1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6">
+                                        <title>External Link</title>
+                                        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                                        <polyline points="15 3 21 3 21 9"></polyline>
+                                        <line x1="10" y1="14" x2="21" y2="3"></line>
+                                    </svg>
+                                </div>
+                            </a>
                         </div>
                     </div>
                     <div className='project-image-first '>
@@ -108,9 +115,7 @@ const ProjectPage = () => {
                             <div className='project-image-wrapper object-fill'>
                                 <img className='w-full h-full opacity-60' src="https://retro-future-flex-525.s3.us-west-1.amazonaws.com/portfolio/devbook.png" />
                             </div>
-
                         </a>
-                        
                     </div>
                 </div>
             </div>
@@ -135,22 +140,24 @@ const ProjectPage = () => {
                         </div>
                         
                         <div className='flex flex-row justify-end'>
-                            <div className='m-3'>
-                                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#a8b2d1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6">
-                                    <title>GitHub</title>
-                                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                                </svg>
-                                
-                            </div>
-                            <div className='ml-3 my-3'>
-                                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#a8b2d1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6">
-                                    <title>External Link</title>
-                                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                                    <polyline points="15 3 21 3 21 9"></polyline>
-                                    <line x1="10" y1="14" x2="21" y2="3"></line>
-                                </svg>
-                                
-                            </div>
+                            <a href="https://github.com/rsato1007/wayfarer-project">
+                                <div className='m-3'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#a8b2d1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6">
+                                        <title>GitHub</title>
+                                        <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                                    </svg>
+                                </div>
+                            </a>
+                            <a href="https://historic-stops.herokuapp.com/">
+                                <div className='ml-3 my-3'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#a8b2d1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6">
+                                        <title>External Link</title>
+                                        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                                        <polyline points="15 3 21 3 21 9"></polyline>
+                                        <line x1="10" y1="14" x2="21" y2="3"></line>
+                                    </svg>
+                                </div>
+                            </a>
                         </div>
                     </div>
                     <div className='project-image-second '>
@@ -186,22 +193,24 @@ const ProjectPage = () => {
                         </div>
                         
                         <div className='flex flex-row justify-start'>
-                            <div className='my-3 mr-3'>
-                                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#a8b2d1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6">
-                                    <title>GitHub</title>
-                                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                                </svg>
-                                
-                            </div>
-                            <div className='m-3'>
-                                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#a8b2d1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6">
-                                    <title>External Link</title>
-                                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                                    <polyline points="15 3 21 3 21 9"></polyline>
-                                    <line x1="10" y1="14" x2="21" y2="3"></line>
-                                </svg>
-                                
-                            </div>
+                            <a href="https://github.com/vincelaq/tron-lightcycle-game">
+                                <div className='my-3 mr-3'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#a8b2d1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6">
+                                        <title>GitHub</title>
+                                        <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                                    </svg>
+                                </div>
+                            </a>
+                            <a href="https://vincelaq.github.io/tron-lightcycle-game/">
+                                <div className='m-3'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#a8b2d1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6">
+                                        <title>External Link</title>
+                                        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                                        <polyline points="15 3 21 3 21 9"></polyline>
+                                        <line x1="10" y1="14" x2="21" y2="3"></line>
+                                    </svg>
+                                </div>
+                            </a>
                         </div>
                     </div>
                     <div className='project-image-first '>
